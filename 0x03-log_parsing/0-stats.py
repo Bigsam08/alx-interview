8ab9b421127c57bad3c96007059805eb71d51c9c
bigsam08 <uelsam994@gmail.com>
2024-10-27 22:14:30 +0100

    done task

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''pass loger count, status codes and error
+handlingling + ctrl C after 10 lines '''
+import sys
+
+
+def print_stats(stats, file_size):
+    print("File size: {:d}".format(file_size))
+    for code, count in sorted(stats.items()):
+        if count:
+            print("{}: {}".format(code, count))
+
+
+if __name__ == "__main__":
+    count = 0
+    file_size = 0
+    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    stats = {code: 0 for code in codes}
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except IndexError:
+                pass
+
+            try:
+                file_size += int(data[-1])
+            except (IndexError, ValueError):
+                pass
+
+            if count % 10 == 0:
+                print_stats(stats, file_size)
+        print_stats(stats, file_size)
+
+    except KeyboardInterrupt:
+        print_stats(stats, file_size)
+        raise
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,11 @@
+# This is a pyton project that focuses on parsing and processing data stream in real time
+# reading from sys.stdin, handling data in specific format
+# calculating base on input data 
+
+# signal handling 
+# data processing 
+# regular expression
+# dictionaries in Python
+# Python exceptions.
+
+# This is an Alx inteview Question
