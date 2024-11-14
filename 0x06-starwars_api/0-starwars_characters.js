bdcb0f94a0bc0373f3fc598d1ab5254376d49ade
bigsam08 <uelsam994@gmail.com>
2024-11-14 16:28:01 +0100

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,30 +3,26 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const apiurl = `https://swapi.dev/api/films/${movieId}/`;
 
-function getMovieCharacters (movieId) {
-  request(apiurl, { json: true }, (error, response, body) => {
+function getMovieCharacters (characters, idx) {
+  if (idx === characters.length) return;
+
+  request(characters[idx], function (error, response, body) {
     if (error || response.statusCode !== 200) {
-      console.error(`Failed to retrive data for movie ID ${movieId}`);
-      return;
+      console.error(`Failed to retrive data for movie ID ${movieId}:`, error);
+    } else {
+      console.log(JSON.parse(body).name);
+      getMovieCharacters(characters, idx + 1);
     }
-    const charURLs = body.characters;
-
-    charURLs.forEach((charURL) => {
-      request(charURL, { json: true }, (CharErr, charRes, charbody) => {
-        if (CharErr || charRes.statusCode !== 200) {
-          console.error(`Failed to retrive character at ${charURLs}`);
-          return;
-        }
-        console.log(charbody.name);
-      });
-    });
   });
 }
 
-if (!movieId) {
-  console.log('invalid Usage!! Usage: ./0-starwars_characters.js < movie id >');
-} else {
-  getMovieCharacters(movieId);
-}
+const apiurl = `https://swapi.dev/api/films/${movieId}/`;
+request(apiurl, function (err, response, body) {
+  if (err || response.statusCode !== 200) {
+    console.error(`Error fetching movie with that ID ${movieId}`, err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    getMovieCharacters(characters, 0);
+  }
+});
