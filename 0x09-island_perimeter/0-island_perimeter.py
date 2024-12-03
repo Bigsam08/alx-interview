c6196357baf34da17f0d73ea616293f746c62738
bigsam08 <uelsam994@gmail.com>
2024-12-03 23:44:05 +0100

    redone

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,21 +3,23 @@
 
 
 def island_perimeter(grid):
+    ''' perimeter of an island'''
+    if not isinstance(grid, list):
+        return 0
     perimeter = 0
     rows = len(grid)
-    cols = len(grid[0])
+    column = len(grid[0]) if rows > 0 else 0
 
     for i in range(rows):
-        for j in range(cols):
+        for j in range(column):
             if grid[i][j] == 1:
-                # Check all four directions
-                if i == 0 or grid[i-1][j] == 0:  # Up
+                ''' check sides'''
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if i == rows-1 or grid[i+1][j] == 0:  # Down
+                if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if j == 0 or grid[i][j-1] == 0:  # Left
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if j == cols-1 or grid[i][j+1] == 0:  # Right
+                if j == column - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-
     return perimeter
